0af8dd9f64799ce4251c721f35c80aab75dcd2a2
izabelle wilding <izabellewilding@gmail.com>
2020-08-02 13:43:57 +0100

    Smooth scroll on click arrow animation

--- src/index.js
+++ src/index.js
@@ -7,4 +7,45 @@ document
     document.getElementById("hamburger").classList.toggle("is-active");
   });
 
-//hamburger animation
+//arrow navigation
+//Code based on https://jsfiddle.net/donu9wsc/
+document.querySelectorAll(".smooth-scroll").forEach((anchor) => {
+  anchor.onclick = (e) => {
+    e.preventDefault();
+    const href = anchor.getAttribute("href");
+    const target = document.querySelector(href);
+    const to = target.offsetTop;
+    scrollTo(document.documentElement, to, 1000);
+  };
+});
+
+const scrollTo = (element, to, duration) => {
+  let start = element.offsetTop;
+  let change = to - start;
+  let currentTime = 0;
+  let increment = 20;
+
+  const animateScroll = () => {
+    currentTime += increment;
+    const val = easeInOutQuad(currentTime, start, change, duration);
+    element.scrollTop = val;
+    if (currentTime < duration) {
+      setTimeout(animateScroll, increment);
+    }
+  };
+
+  animateScroll();
+};
+
+// Easing function -> easeInOutQuad
+//
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+const easeInOutQuad = (t, b, c, d) => {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t + b;
+  t--;
+  return (-c / 2) * (t * (t - 2) - 1) + b;
+};
